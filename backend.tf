37b5887abcf933989d712678974c0b0d9a5fe3fe
gricasaws <labs.gc.aws@gmail.com>
2020-08-17 11:07:29 -0700

    adding new bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devops-bucket-244894825676"
+    bucket = "devops-bucket-gricas"
     key    = "myec2/ec2-from-terraform.tfstate"
     region = "us-east-1"
   }
